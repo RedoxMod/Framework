d81b4b668fbd568c7285664465c28169d0c67ed9
Ice Cold <wargamingnl7@gmail.com>
2020-10-25 16:16:04 +0100

    Forgot to add some files.

---
+++ Redox.Core/Plugins/PluginManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Redox.API.Components;
+using Redox.API.Plugins;
+
+namespace Redox.Core.Plugins
+{
+    [ComponentInfo("PluginManager", LoadPriority.Low)]
+    public sealed class PluginManager : IPluginManager
+    {
+        private readonly Dictionary<string, IBasePlugin> _plugins = new Dictionary<string, IBasePlugin>();
+        
+        public void AddPlugin(IBasePlugin plugin)
+        {
+            if (plugin == null)
+                return;
+            
+            string title = plugin.Info.Title;
+            if (!_plugins.ContainsKey(title))
+            {
+                _plugins.Add(title, plugin);
+            }
+        }
+        
+        public void RemovePlugin(IBasePlugin plugin)
+        {
+            if (plugin == null)
+                return;
+            string title = plugin.Info.Title;
+            if (_plugins.ContainsKey(title))
+            {
+                _plugins.Remove(title);
+            }
+        }
+
+        public async Task LoadPlugin(string name)
+        {
+            
+        }
+
+        public async Task ReloadPlugin(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task UnloadPlugin(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task RunAsync()
+        {
+        }
+
+        public async Task ShutdownAsync()
+        {
+        }
+    }
+}
---
+++ Redox.Core/RedoxMod.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Redox.API;
+using Redox.API.Commands;
+using Redox.API.Components;
+using Redox.API.Configuration;
+using Redox.API.Database;
+using Redox.API.Engines;
+using Redox.API.Plugins;
+using Redox.API.Roles;
+
+using Redox.Core.Commands;
+using Redox.Core.Components;
+using Redox.Core.Configuration;
+using Redox.Core.Configuration.Redox;
+using Redox.Core.Database;
+using Redox.Core.Engines;
+using Redox.Core.Http;
+using Redox.Core.Parsers;
+using Redox.Core.Plugins;
+using Redox.Core.Roles;
+
+namespace Redox.Core
+{
+    public sealed class RedoxMod
+    {
+        private static RedoxMod _instance;
+
+        public static RedoxMod GetMod()
+        {
+            return _instance ??= new RedoxMod();
+        }
+        
+        public readonly string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        
+        
+        #region Properties
+        
+        public RedoxConfig Config { get; } = new RedoxConfig();
+        
+        #region Directories
+
+        public string RootDirectory { get; } = "RedoxMod\\";
+        public string PluginDirectory { get; private set; }
+        public string DataDirectory { get; private set; }
+        public string LoggingDirectory { get; private set; }
+
+        public string DependenciesDirectory {get; private set;}
+        #endregion
+
+        #region Components
+
+        public IComponentProvider Components => ComponentProvider.Get();
+
+        public ILogger Logger => Components.ResolveComponent<ILogger>();
+
+        public IRolesProvider RolesProvider => Components.ResolveComponent<IRolesProvider>();
+        
+        public IPluginEngineProvider PluginEngineProvider => Components.ResolveComponent<IPluginEngineProvider>();
+
+        public IPluginManager PluginManager => Components.ResolveComponent<IPluginManager>();
+        
+        #endregion
+        
+        #endregion
+        
+        public async Task InitializeAsync()
+        {
+            PluginDirectory = "Plugins";
+            DataDirectory = "Data";
+            LoggingDirectory = "Logs";
+            DependenciesDirectory = "Dependencies";
+            if (!Directory.Exists(RootDirectory)) Directory.CreateDirectory(RootDirectory);
+            Directory.SetCurrentDirectory(RootDirectory);
+            if (!Directory.Exists(PluginDirectory)) Directory.CreateDirectory(PluginDirectory);
+            if (!Directory.Exists(DataDirectory)) Directory.CreateDirectory(DataDirectory);
+            if (!Directory.Exists(LoggingDirectory)) Directory.CreateDirectory(LoggingDirectory);
+            if (!Directory.Exists(DependenciesDirectory)) Directory.CreateDirectory(DependenciesDirectory);
+            
+            //Used for local testing. Will be removed in the future.
+            File.WriteAllText("redox.config.xml", XmlParser.ToXml(Config.Init()));
+            this.RegisterComponents();
+        }
+
+        private async void RegisterComponents()
+        {
+            Components.RegisterType<ILogger, Logger>();
+            Components.RegisterType<IRolesProvider, RolesProvider>();
+            Components.RegisterType<IPermissionsProvider, PermissionsProvider>();
+            Components.RegisterType<IConfigurationProvider, ConfigurationProvider>();
+            Components.RegisterType<IPluginEngineProvider, PluginEngineProvider>();
+            Components.RegisterType<IPluginManager, PluginManager>();
+            Components.RegisterType<ICommandProvider, CommandProvider>();
+
+            await Components.StartAllAsync();
+        }
+
+        public async Task ShutdownAsync()
+        {
+            
+        }
+    }
+}
